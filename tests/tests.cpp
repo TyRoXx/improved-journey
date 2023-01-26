c6b57136096824df3caed91c897b7f97d81192fc
TyRoXx <tyroxxxx@gmail.com>
2023-01-26 18:49:09 +0100

    Simplify test by using GENERATE

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,10 +1,10 @@
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
 #include <ij/direction.h>
 
 TEST_CASE("Directions and vectors round trip", "[direction]")
 {
-    CHECK(ij::Direction::Up == ij::DirectionFromVector(ij::DirectionToVector(ij::Direction::Up)));
-    CHECK(ij::Direction::Left == ij::DirectionFromVector(ij::DirectionToVector(ij::Direction::Left)));
-    CHECK(ij::Direction::Down == ij::DirectionFromVector(ij::DirectionToVector(ij::Direction::Down)));
-    CHECK(ij::Direction::Right == ij::DirectionFromVector(ij::DirectionToVector(ij::Direction::Right)));
+    const ij::Direction direction =
+        GENERATE(ij::Direction::Up, ij::Direction::Left, ij::Direction::Down, ij::Direction::Right);
+    CHECK(direction == ij::DirectionFromVector(ij::DirectionToVector(direction)));
 }
